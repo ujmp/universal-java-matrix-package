0ce2016067b19c461d312c58b2223986460d590c
jdmp <jdmp@86589847-9783-470e-86a4-a9a86fc44553>
2008-11-14 15:35:18 +0000

    license added

--- ujmp-itext/trunk/src/test/java/org/ujmp/itext/TestExportPDF.java
+++ ujmp-itext/trunk/src/test/java/org/ujmp/itext/TestExportPDF.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (C) 2008 Holger Arndt, Andreas Naegele and Markus Bundschus
  *
- * This file is part of the Java Data Mining Package (JDMP).
+ * This file is part of the Universal Java Matrix Package (UJMP).
  * See the NOTICE file distributed with this work for additional
  * information regarding copyright ownership and licensing.
  *
- * JDMP is free software; you can redistribute it and/or modify
+ * UJMP is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- * JDMP is distributed in the hope that it will be useful,
+ * UJMP is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with JDMP; if not, write to the
+ * License along with UJMP; if not, write to the
  * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
  * Boston, MA  02110-1301  USA
  */

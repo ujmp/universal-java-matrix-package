4677dc9dc849d68e3353ffe377ffc049f59ba33f
jdmp <jdmp@86589847-9783-470e-86a4-a9a86fc44553>
2008-11-21 09:06:46 +0000

    append added for FileWriter

--- ujmp-core/trunk/src/main/java/org/ujmp/core/util/io/IntelligentFileWriter.java
+++ ujmp-core/trunk/src/main/java/org/ujmp/core/util/io/IntelligentFileWriter.java
@@ -48,25 +48,39 @@ public class IntelligentFileWriter extends Writer implements Appendable, Closeab
 	private BufferedWriter bw = null;
 
 	public IntelligentFileWriter(String filename) throws IOException, ClassNotFoundException {
-		this(new File(filename));
+		this(filename, false);
+	}
+
+	public IntelligentFileWriter(String filename, boolean append) throws IOException,
+			ClassNotFoundException {
+		this(new File(filename), append);
 	}
 
 	public IntelligentFileWriter(OutputStream outputStream) throws IOException {
+		this(outputStream, false);
+	}
+
+	public IntelligentFileWriter(OutputStream outputStream, boolean append) throws IOException {
 		bw = new BufferedWriter(new OutputStreamWriter(outputStream));
 	}
 
 	public IntelligentFileWriter(File file) throws IOException {
+		this(file, false);
+	}
+
+	public IntelligentFileWriter(File file, boolean append) throws IOException {
 		if (file.getAbsolutePath().toLowerCase().endsWith(".gz")) {
-			zip = new GZIPOutputStream(new FileOutputStream(file));
+			zip = new GZIPOutputStream(new FileOutputStream(file, append));
 			bw = new BufferedWriter(new OutputStreamWriter(zip));
 		} else if (file.getAbsolutePath().toLowerCase().endsWith(".z")) {
-			zip = new ZipOutputStream(new FileOutputStream(file));
+			zip = new ZipOutputStream(new FileOutputStream(file, append));
 			bw = new BufferedWriter(new OutputStreamWriter(zip));
 		} else if (file.getAbsolutePath().toLowerCase().endsWith(".7z")) {
 			try {
 				Class<?> c = Class.forName(SEVENZIPOUTPUTSTREAM);
 				Constructor con = c.getConstructor(new Class[] { FileOutputStream.class });
-				zip = (OutputStream) con.newInstance(new Object[] { new FileOutputStream(file) });
+				zip = (OutputStream) con.newInstance(new Object[] { new FileOutputStream(file,
+						append) });
 			} catch (ClassNotFoundException e) {
 				throw new IOException("Could not find ujmp-j7zip in classpath", e);
 			} catch (Exception e) {
@@ -74,13 +88,19 @@ public class IntelligentFileWriter extends Writer implements Appendable, Closeab
 			}
 			bw = new BufferedWriter(new OutputStreamWriter(zip));
 		} else {
-			fw = new FileWriter(file);
+			fw = new FileWriter(file, append);
 			bw = new BufferedWriter(fw);
 		}
 	}
 
 	public static final void save(String filename, String text) throws Exception {
-		IntelligentFileWriter fw = new IntelligentFileWriter(filename);
+		IntelligentFileWriter fw = new IntelligentFileWriter(filename, false);
+		fw.write(text);
+		fw.close();
+	}
+
+	public static final void append(String filename, String text) throws Exception {
+		IntelligentFileWriter fw = new IntelligentFileWriter(filename, true);
 		fw.write(text);
 		fw.close();
 	}

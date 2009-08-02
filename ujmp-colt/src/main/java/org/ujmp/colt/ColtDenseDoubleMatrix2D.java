71f540265cbdda1fb052fc6c2e78e0fda60b12b3
Holger Arndt <holger-arndt@users.noreply.github.com>
2009-08-02 16:04:35 +0000

    moved

---
+++ ujmp-colt/src/main/java/org/ujmp/colt/ColtDenseDoubleMatrix2D.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2008 Holger Arndt, Andreas Naegele and Markus Bundschus
+ *
+ * This file is part of the Universal Java Matrix Package (UJMP).
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * UJMP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * UJMP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with UJMP; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.ujmp.colt;
+
+import org.ujmp.core.Matrix;
+import org.ujmp.core.doublematrix.AbstractDenseDoubleMatrix2D;
+import org.ujmp.core.exceptions.MatrixException;
+import org.ujmp.core.interfaces.Wrapper;
+
+import cern.colt.matrix.impl.DenseDoubleMatrix2D;
+
+public class ColtDenseDoubleMatrix2D extends AbstractDenseDoubleMatrix2D implements Wrapper<DenseDoubleMatrix2D> {
+	private static final long serialVersionUID = -3223474248020842822L;
+
+	private DenseDoubleMatrix2D matrix = null;
+
+	public ColtDenseDoubleMatrix2D(long... size) {
+		this.matrix = new DenseDoubleMatrix2D((int) size[ROW], (int) size[COLUMN]);
+	}
+
+	public ColtDenseDoubleMatrix2D(DenseDoubleMatrix2D m) {
+		this.matrix = m;
+	}
+
+	public ColtDenseDoubleMatrix2D(Matrix source) throws MatrixException {
+		this(source.getSize());
+		for (long[] c : source.availableCoordinates()) {
+			setAsDouble(source.getAsDouble(c), c);
+		}
+	}
+
+	public double getDouble(long row, long column) {
+		return matrix.getQuick((int) row, (int) column);
+	}
+
+	public long[] getSize() {
+		return new long[] { matrix.rows(), matrix.columns() };
+	}
+
+	public void setDouble(double value, long row, long column) {
+		matrix.setQuick((int) row, (int) column, value);
+	}
+
+	public DenseDoubleMatrix2D getWrappedObject() {
+		return matrix;
+	}
+
+	public void setWrappedObject(DenseDoubleMatrix2D object) {
+		this.matrix = object;
+	}
+
+}

95f46b0e5eeaccc184c217ba57bb3b0669f6ded2
Holger Arndt <holger-arndt@users.noreply.github.com>
2009-08-02 16:53:34 +0000

    moved

---
+++ ujmp-gui/src/main/java/org/ujmp/gui/UJMP.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2008-2009 Holger Arndt, A. Naegele and M. Bundschus
+ *
+ * This file is part of the Universal Java Matrix Package (UJMP).
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * UJMP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * UJMP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with UJMP; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.ujmp.gui;
+
+import org.ujmp.core.Matrix;
+import org.ujmp.core.MatrixFactory;
+import org.ujmp.core.enums.ValueType;
+
+public class UJMP {
+
+	public static final String UJMPVERSION = org.ujmp.core.UJMP.UJMPVERSION;
+
+	public static void main(String[] args) {
+
+		Matrix m = MatrixFactory.zeros(ValueType.OBJECT, 13, 9);
+
+		m.setLabel("Welcome to UJMP");
+
+		m.setAsObject("Please visit", 2, 3);
+		m.setAsObject("http://www.ujmp.org/", 2, 4);
+		m.setAsObject("for more info", 2, 5);
+
+		m.setAsObject("Welcome", 3, 1);
+		m.setAsObject("to the", 3, 2);
+		m.setAsObject("Universal", 3, 3);
+		m.setAsObject("Java", 3, 4);
+		m.setAsObject("Matrix", 3, 5);
+		m.setAsObject("Package", 3, 6);
+		m.setAsObject("UJMP", 3, 7);
+
+		m.setAsObject(100, 5, 2);
+		m.setAsObject(100, 5, 6);
+
+		m.setAsObject(1, 7, 4);
+
+		m.setAsObject(-1, 9, 2);
+		m.setAsObject(-1, 9, 6);
+		m.setAsObject(-1, 10, 3);
+		m.setAsObject(-1, 10, 4);
+		m.setAsObject(-1, 10, 5);
+
+		m.showGUI();
+	}
+
+}

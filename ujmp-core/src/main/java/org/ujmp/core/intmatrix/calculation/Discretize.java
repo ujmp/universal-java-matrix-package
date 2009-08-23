0bdfee601dc4a858f04ffecd4364dcc771efab74
Holger Arndt <holger-arndt@users.noreply.github.com>
2009-08-23 23:44:53 +0000

    discretization added

---
+++ ujmp-core/src/main/java/org/ujmp/core/intmatrix/calculation/Discretize.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2008-2009 Holger Arndt, A. Naegele and M. Bundschus
+ *
+ * This file is part of the Universal Java Matrix Package (UJMP).
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * UJMP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * UJMP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with UJMP; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.ujmp.core.intmatrix.calculation;
+
+import org.ujmp.core.Matrix;
+import org.ujmp.core.doublematrix.calculation.entrywise.rounding.Round;
+import org.ujmp.core.exceptions.MatrixException;
+
+public class Discretize extends AbstractIntCalculation {
+	private static final long serialVersionUID = -2045926868254834270L;
+
+	private Matrix discretized = null;
+
+	public enum DiscretizationMethod {
+		ROUND, STRINGS, STANDARDBINNING, EQUALBINNING, INFORMATIONGAIN
+	};
+
+	public Discretize(Matrix matrix, DiscretizationMethod method, int numberOfBins) {
+		super(matrix);
+		switch (method) {
+		case ROUND:
+			discretized = new Round(matrix).calcLink();
+			break;
+		default:
+			throw new MatrixException("method not yet implemented");
+		}
+	}
+
+	public int getInt(long... coordinates) throws MatrixException {
+		return discretized.getAsInt(coordinates);
+	}
+
+}

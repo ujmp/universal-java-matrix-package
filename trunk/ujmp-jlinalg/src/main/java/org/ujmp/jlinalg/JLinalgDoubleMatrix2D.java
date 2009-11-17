10eb2005e099947e47aeb3a44f624be7a3666769
jdmp <jdmp@86589847-9783-470e-86a4-a9a86fc44553>
2009-11-17 22:41:46 +0000

    jlinalg added

---
+++ trunk/ujmp-jlinalg/src/main/java/org/ujmp/jlinalg/JLinalgDoubleMatrix2D.java
@@ -0,0 +1,13 @@
+package org.ujmp.jlinalg;
+
+/**
+ * Hello world!
+ *
+ */
+public class JLinalgDoubleMatrix2D 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

f3cf3bac20b9ebf4ffb813c8cfea314ca72fe00e
jdmp <jdmp@86589847-9783-470e-86a4-a9a86fc44553>
2010-01-15 17:21:55 +0000

    bf

--- trunk/ujmp-vecmath/src/main/java/org/ujmp/vecmath/VecMathDenseDoubleMatrix2D.java
+++ trunk/ujmp-vecmath/src/main/java/org/ujmp/vecmath/VecMathDenseDoubleMatrix2D.java
@@ -27,6 +27,7 @@ import javax.vecmath.GMatrix;
 import javax.vecmath.GVector;
 
 import org.ujmp.core.Matrix;
+import org.ujmp.core.calculation.Calculation.Ret;
 import org.ujmp.core.doublematrix.DenseDoubleMatrix2D;
 import org.ujmp.core.doublematrix.stub.AbstractDenseDoubleMatrix2D;
 import org.ujmp.core.exceptions.MatrixException;
@@ -87,10 +88,41 @@ public class VecMathDenseDoubleMatrix2D extends AbstractDenseDoubleMatrix2D
 		this.matrix = object;
 	}
 
-	public DenseDoubleMatrix2D transpose() {
+	public VecMathDenseDoubleMatrix2D transpose() {
 		GMatrix m = (GMatrix) matrix.clone();
 		m.transpose();
 		return new VecMathDenseDoubleMatrix2D(m);
+	}
+
+	public Matrix plus(Matrix m) {
+		if (m instanceof VecMathDenseDoubleMatrix2D) {
+			GMatrix result = (GMatrix) matrix.clone();
+			result.add(((VecMathDenseDoubleMatrix2D) m).matrix);
+			return new VecMathDenseDoubleMatrix2D(result);
+		} else {
+			return super.plus(m);
+		}
+	}
+
+	public Matrix minus(Matrix m) {
+		if (m instanceof VecMathDenseDoubleMatrix2D) {
+			GMatrix result = (GMatrix) matrix.clone();
+			result.sub(((VecMathDenseDoubleMatrix2D) m).matrix);
+			return new VecMathDenseDoubleMatrix2D(result);
+		} else {
+			return super.minus(m);
+		}
+	}
+
+	public Matrix mtimes(Matrix m) {
+		if (m instanceof VecMathDenseDoubleMatrix2D) {
+			GMatrix result = new GMatrix(matrix.getNumRow(), (int) m
+					.getColumnCount());
+			result.mul(matrix, ((VecMathDenseDoubleMatrix2D) m).matrix);
+			return new VecMathDenseDoubleMatrix2D(result);
+		} else {
+			return super.mtimes(m);
+		}
 	}
 
 	public DenseDoubleMatrix2D inv() {
@@ -99,6 +131,7 @@ public class VecMathDenseDoubleMatrix2D extends AbstractDenseDoubleMatrix2D
 		return new VecMathDenseDoubleMatrix2D(m);
 	}
 
+	// in S all entries on the diagonal are 1
 	public Matrix[] svd() {
 		GMatrix m = (GMatrix) matrix.clone();
 		int nrows = (int) getRowCount();
@@ -113,13 +146,24 @@ public class VecMathDenseDoubleMatrix2D extends AbstractDenseDoubleMatrix2D
 		return new Matrix[] { U, S, V };
 	}
 
+	// non-singular matrices only
 	public Matrix[] lu() {
 		if (isSquare()) {
 			GMatrix m = (GMatrix) matrix.clone();
-			GMatrix l = (GMatrix) matrix.clone();
-			GVector v = new GVector(matrix.getNumCol());
-			m.LUD(l, v);
-			return new Matrix[] {};
+			GMatrix lu = (GMatrix) matrix.clone();
+			GVector piv = new GVector(matrix.getNumCol());
+			m.LUD(lu, piv);
+			Matrix l = new VecMathDenseDoubleMatrix2D(lu).tril(Ret.NEW, 0);
+			for (int i = (int) l.getRowCount() - 1; i != -1; i--) {
+				l.setAsDouble(1, i, i);
+			}
+			Matrix u = new VecMathDenseDoubleMatrix2D(lu).triu(Ret.NEW, 0);
+			VecMathDenseDoubleMatrix2D p = new VecMathDenseDoubleMatrix2D(
+					getSize());
+			for (int i = piv.getSize() - 1; i != -1; i--) {
+				p.setDouble(1, i, (int) piv.getElement(i));
+			}
+			return new Matrix[] { l, u, p };
 		} else {
 			throw new MatrixException("only allowed for square matrices");
 		}

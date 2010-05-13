d71e3d7b6c530513a0f4b484f9036d5cfc261e46
Holger Arndt <holger-arndt@users.noreply.github.com>
2010-05-13 01:53:20 +0000

    version updates and licenses added

--- ujmp-pdfbox/src/test/java/org/ujmp/pdfbox/AllTests.java
+++ ujmp-pdfbox/src/test/java/org/ujmp/pdfbox/AllTests.java
@@ -27,6 +27,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses( {})
+@Suite.SuiteClasses( { TestImportPDF.class })
 public class AllTests {
 }

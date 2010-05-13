82bd34896dc77719a8bc182699815b103fe0c2d2
jdmp <jdmp@86589847-9783-470e-86a4-a9a86fc44553>
2010-05-13 01:53:20 +0000

    version updates and licenses added

---
+++ trunk/ujmp-core/src/main/java/org/ujmp/core/util/ResourceUtil.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2008-2010 by Holger Arndt
+ *
+ * This file is part of the Universal Java Matrix Package (UJMP).
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * UJMP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * UJMP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with UJMP; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.ujmp.core.util;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public abstract class ResourceUtil {
+
+	public static void copyToFile(String resource, File file) throws Exception {
+		InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream(resource);
+		FileOutputStream os = new FileOutputStream(file);
+
+		BufferedInputStream bis = new BufferedInputStream(is);
+		BufferedOutputStream bos = new BufferedOutputStream(os);
+
+		byte buf[] = new byte[8192];
+		int len;
+		while ((len = bis.read(buf)) > 0) {
+			bos.write(buf, 0, len);
+		}
+
+		bis.close();
+		bos.close();
+
+		is.close();
+		os.close();
+	}
+
+}

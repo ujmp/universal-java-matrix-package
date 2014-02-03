cc96d24582a378df6e73de4863807528a3f3e36d
Holger Arndt <holger-arndt@users.noreply.github.com>
2014-02-03 16:04:05 +0000

    Share project "ujmp-examples" into "https://svn.code.sf.net/p/ujmp/code"

---
+++ ujmp-examples/src/main/java/org/ujmp/examples/RandomMatrixExample.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2008-2014 by Holger Arndt
+ *
+ * This file is part of the Universal Java Matrix Package (UJMP).
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * UJMP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * UJMP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with UJMP; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+package org.ujmp.examples;
+
+import org.ujmp.core.Matrix;
+
+public class RandomMatrixExample {
+
+	public static void main(String[] args) throws Exception {
+
+		// create matrix with random values between 0 and 1
+		Matrix rand = Matrix.Factory.rand(100, 10);
+
+		// create matrix with random values between -1 and - 1
+		Matrix randn = Matrix.Factory.randn(100, 10);
+
+		// show on screen
+		rand.showGUI();
+		randn.showGUI();
+	}
+}

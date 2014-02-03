8761c5eaf1651c56daa9ce58ceb00ee552429946
Holger Arndt <holger-arndt@users.noreply.github.com>
2014-02-03 15:54:01 +0000

    refactoring

---
+++ ujmp-jung/src/main/java/org/ujmp/jung/ColorTransformer.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2008-2014 by Holger Arndt
+ *
+ * This file is part of the Universal Java Matrix Package (UJMP).
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership and licensing.
+ *
+ * UJMP is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * UJMP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with UJMP; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+
+package org.ujmp.jung;
+
+import java.awt.Color;
+import java.awt.Paint;
+
+import org.apache.commons.collections15.Transformer;
+import org.ujmp.gui.util.ColorUtil;
+
+import edu.uci.ics.jung.visualization.picking.PickedState;
+
+public class ColorTransformer<T> implements Transformer<T, Paint> {
+
+	private PickedState<T> pickedState = null;
+
+	public ColorTransformer(PickedState<T> pickedState) {
+		this.pickedState = pickedState;
+	}
+
+	public ColorTransformer() {
+	}
+
+	public Paint transform(T input) {
+		if (pickedState == null) {
+			return ColorUtil.fromObject(input);
+		} else if (pickedState.isPicked(input)) {
+			return new Color(200, 200, 255);
+		} else {
+			return ColorUtil.fromObject(input);
+		}
+	}
+
+}

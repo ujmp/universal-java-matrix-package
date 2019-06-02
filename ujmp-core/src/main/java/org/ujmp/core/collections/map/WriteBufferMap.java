daf076707e6e355e21224d57466d16c342620486
Holger Arndt <mail@holger-arndt.com>
2019-06-02 13:37:06 +0200

    create WriteBufferMap

---
+++ ujmp-core/src/main/java/org/ujmp/core/collections/map/WriteBufferMap.java
@@ -0,0 +1,152 @@
+package org.ujmp.core.collections.map;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class WriteBufferMap<K, V> extends AbstractMap<K, V> implements Closeable {
+
+    private final Map<K, V> writeTarget;
+    private final Map<K, V> writeBuffer;
+    private final WriteThread writeThread;
+    private int commitInterval = 1000;
+    private int maxWriteBufferSize = 100000;
+    private int maxBatchSize = 10000;
+    private boolean isClosed = false;
+
+    public WriteBufferMap(Map<K, V> writeTarget, Map<K, V> writeBuffer) {
+        this.writeTarget = writeTarget;
+        this.writeBuffer = writeBuffer;
+        this.writeThread = new WriteThread(this, writeTarget, writeBuffer);
+    }
+
+
+    public WriteBufferMap(Map<K, V> map) {
+        this(map, new HashMap<>());
+    }
+
+    @Override
+    public void clear() {
+        synchronized (writeBuffer) {
+            writeBuffer.clear();
+            writeTarget.clear();
+        }
+    }
+
+    @Override
+    public V get(Object key) {
+        V value1 = writeBuffer.get(key);
+        V value2 = writeTarget.get(key);
+        if (value1 != null) {
+            return value1;
+        } else {
+            return value2;
+        }
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return writeTarget.keySet();
+    }
+
+    @Override
+    public V put(K key, V value) {
+        while (writeBuffer.size() >= maxWriteBufferSize) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException ignored) {
+            }
+        }
+        synchronized (writeBuffer) {
+            return writeBuffer.put(key, value);
+        }
+    }
+
+    @Override
+    public V remove(Object key) {
+        V value1;
+        synchronized (writeBuffer) {
+            value1 = writeBuffer.remove(key);
+        }
+        V value2 = writeTarget.remove(key);
+        if (value1 != null) {
+            return value1;
+        } else {
+            return value2;
+        }
+    }
+
+    @Override
+    public int size() {
+        return writeBuffer.size() + writeTarget.size();
+    }
+
+    public int getCommitInterval() {
+        return commitInterval;
+    }
+
+    public int getMaxWriteBufferSize() {
+        return maxWriteBufferSize;
+    }
+
+    public boolean isClosed() {
+        return isClosed;
+    }
+
+    @Override
+    public void close() throws IOException {
+        if (writeTarget instanceof Closeable) {
+            ((Closeable) writeTarget).close();
+        }
+        isClosed = true;
+    }
+
+    public int getMaxBatchSize() {
+        return maxBatchSize;
+    }
+}
+
+class WriteThread<K, V> extends Thread {
+
+    private final WriteBufferMap<K, V> writeBufferMap;
+    private final Map<K, V> writeBuffer;
+    private final Map<K, V> writeTarget;
+    private long lastRunTime = 0;
+    private final Map<K, V> currentBatch = new HashMap<>();
+
+
+    public WriteThread(WriteBufferMap<K, V> writeBufferMap, Map<K, V> writeTarget, Map<K, V> writeBuffer) {
+        this.writeBufferMap = writeBufferMap;
+        this.writeTarget = writeTarget;
+        this.writeBuffer = writeBuffer;
+        this.start();
+    }
+
+    public void run() {
+        while (!writeBufferMap.isClosed()) {
+            while (System.currentTimeMillis() - lastRunTime < writeBufferMap.getCommitInterval() && writeBuffer.size() < writeBufferMap.getMaxWriteBufferSize()) {
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            lastRunTime = System.currentTimeMillis();
+            while (!writeBuffer.isEmpty() && currentBatch.size() < writeBufferMap.getMaxBatchSize()) {
+                K key;
+                V value;
+                synchronized (writeBuffer) {
+                    key = writeBuffer.keySet().iterator().next();
+                    value = writeBuffer.remove(key);
+                }
+                currentBatch.put(key, value);
+            }
+            if (!currentBatch.isEmpty()) {
+                writeTarget.putAll(currentBatch);
+                currentBatch.clear();
+            }
+        }
+    }
+}
--- ujmp-core/src/main/java/org/ujmp/core/mapmatrix/AbstractMapMatrix.java
+++ ujmp-core/src/main/java/org/ujmp/core/mapmatrix/AbstractMapMatrix.java
@@ -25,13 +25,7 @@ package org.ujmp.core.mapmatrix;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.util.AbstractCollection;
-import java.util.AbstractSet;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import org.ujmp.core.collections.list.FastArrayList;
 import org.ujmp.core.objectmatrix.stub.AbstractDenseObjectMatrix2D;
@@ -39,224 +33,236 @@ import org.ujmp.core.util.MathUtil;
 import org.ujmp.core.util.StringUtil;
 
 public abstract class AbstractMapMatrix<K, V> extends AbstractDenseObjectMatrix2D implements MapMatrix<K, V> {
-	private static final long serialVersionUID = 5571429371462164416L;
+    private static final long serialVersionUID = 5571429371462164416L;
 
-	private volatile boolean isIndexUpToDate = false;
-	private final List<K> keyIndexList = new FastArrayList<K>();
+    private volatile boolean isIndexUpToDate = false;
+    private final List<K> keyIndexList = new FastArrayList<K>();
 
-	public AbstractMapMatrix() {
-		super(0, 2);
-	}
+    public AbstractMapMatrix() {
+        super(0, 2);
+    }
 
-	public synchronized final long[] getSize() {
-		size[ROW] = size();
-		return size;
-	}
+    public synchronized final long[] getSize() {
+        size[ROW] = size();
+        return size;
+    }
 
-	public synchronized final Object getObject(long row, long column) {
-		return getObject(MathUtil.longToInt(row), MathUtil.longToInt(column));
-	}
+    public synchronized final Object getObject(long row, long column) {
+        return getObject(MathUtil.longToInt(row), MathUtil.longToInt(column));
+    }
 
-	public synchronized final Object getObject(int row, int column) {
-		Object mapKey = getKey(row);
-		if (column == 0) {
-			return mapKey;
-		} else if (column == 1) {
-			return (mapKey == null ? null : get(mapKey));
-		} else {
-			return null;
-		}
-	}
+    public synchronized final Object getObject(int row, int column) {
+        Object mapKey = getKey(row);
+        if (column == 0) {
+            return mapKey;
+        } else if (column == 1) {
+            return (mapKey == null ? null : get(mapKey));
+        } else {
+            return null;
+        }
+    }
 
-	@SuppressWarnings("unchecked")
-	public synchronized final void setObject(Object value, int row, int column) {
-		if (column == 0) {
-			// remove old key and add new key with old value
-			K key = getKey(row);
-			V oldValue = get(key);
-			remove(key);
-			put((K) value, oldValue);
-		} else if (column == 1) {
-			K key = getKey(row);
-			put(key, (V) value);
-		}
-	}
+    @SuppressWarnings("unchecked")
+    public synchronized final void setObject(Object value, int row, int column) {
+        if (column == 0) {
+            // remove old key and add new key with old value
+            K key = getKey(row);
+            V oldValue = get(key);
+            remove(key);
+            put((K) value, oldValue);
+        } else if (column == 1) {
+            K key = getKey(row);
+            put(key, (V) value);
+        }
+    }
 
-	public synchronized final void setObject(Object value, long row, long column) {
-		setObject(value, MathUtil.longToInt(row), MathUtil.longToInt(column));
-	}
+    public synchronized final void setObject(Object value, long row, long column) {
+        setObject(value, MathUtil.longToInt(row), MathUtil.longToInt(column));
+    }
 
-	public synchronized MapMatrix<K, V> clone() {
-		buildIndexIfNecessary();
-		MapMatrix<K, V> clone = new DefaultMapMatrix<K, V>();
-		clone.putAll(this);
-		for (K key : keyIndexList) {
-			V value = get(key);
-			clone.put(key, value);
-		}
-		return clone;
-	}
+    public synchronized MapMatrix<K, V> clone() {
+        buildIndexIfNecessary();
+        MapMatrix<K, V> clone = new DefaultMapMatrix<K, V>();
+        clone.putAll(this);
+        for (K key : keyIndexList) {
+            V value = get(key);
+            clone.put(key, value);
+        }
+        return clone;
+    }
 
-	public String getAsString(Object key) {
-		return StringUtil.convert(get(key));
-	}
+    public String getAsString(Object key) {
+        return StringUtil.convert(get(key));
+    }
 
-	private synchronized final K getKey(int index) {
-		buildIndexIfNecessary();
-		K k = null;
-		if (index >= 0 && index < keyIndexList.size()) {
-			k = keyIndexList.get(index);
-		}
-		return k;
-	}
+    private synchronized final K getKey(int index) {
+        buildIndexIfNecessary();
+        K k = null;
+        if (index >= 0 && index < keyIndexList.size()) {
+            k = keyIndexList.get(index);
+        }
+        return k;
+    }
 
-	private synchronized void buildIndexIfNecessary() {
-		if (!isIndexUpToDate) {
-			Iterator<K> it = keySet().iterator();
-			while (it.hasNext()) {
-				keyIndexList.add(it.next());
-			}
-			if (it instanceof Closeable) {
-				try {
-					((Closeable) it).close();
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
-			}
-			isIndexUpToDate = true;
-		}
-	}
+    private synchronized void buildIndexIfNecessary() {
+        if (!isIndexUpToDate) {
+            Iterator<K> it = keySet().iterator();
+            while (it.hasNext()) {
+                keyIndexList.add(it.next());
+            }
+            if (it instanceof Closeable) {
+                try {
+                    ((Closeable) it).close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            isIndexUpToDate = true;
+        }
+    }
 
-	public boolean containsKey(Object key) {
-		return keySet().contains(key);
-	}
+    public boolean containsKey(Object key) {
+        return keySet().contains(key);
+    }
 
-	public synchronized boolean containsValue(Object value) {
-		buildIndexIfNecessary();
-		for (K key : keyIndexList) {
-			if (value.equals(get(key))) {
-				return true;
-			}
-		}
-		return false;
-	}
+    public synchronized boolean containsValue(Object value) {
+        buildIndexIfNecessary();
+        for (K key : keyIndexList) {
+            if (value.equals(get(key))) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	public synchronized Set<java.util.Map.Entry<K, V>> entrySet() {
-		final AbstractMapMatrix<K, V> map = this;
-		return new AbstractSet<Entry<K, V>>() {
+    public synchronized Set<java.util.Map.Entry<K, V>> entrySet() {
+        final AbstractMapMatrix<K, V> map = this;
+        return new AbstractSet<Entry<K, V>>() {
 
-			@Override
-			public Iterator<java.util.Map.Entry<K, V>> iterator() {
-				return new Iterator<Entry<K, V>>() {
+            @Override
+            public Iterator<java.util.Map.Entry<K, V>> iterator() {
+                return new Iterator<Entry<K, V>>() {
 
-					Iterator<K> it = keySet().iterator();
+                    Iterator<K> it = keySet().iterator();
 
-					public boolean hasNext() {
-						return it.hasNext();
-					}
+                    public boolean hasNext() {
+                        return it.hasNext();
+                    }
 
-					public java.util.Map.Entry<K, V> next() {
-						final K k = it.next();
-						final V v = get(k);
-						return new java.util.Map.Entry<K, V>() {
+                    public java.util.Map.Entry<K, V> next() {
+                        final K k = it.next();
+                        final V v = get(k);
+                        return new java.util.Map.Entry<K, V>() {
 
-							public K getKey() {
-								return k;
-							}
+                            public K getKey() {
+                                return k;
+                            }
 
-							public V getValue() {
-								return v;
-							}
+                            public V getValue() {
+                                return v;
+                            }
 
-							public V setValue(V value) {
-								throw new UnsupportedOperationException();
-							}
-						};
-					}
+                            public V setValue(V value) {
+                                throw new UnsupportedOperationException();
+                            }
+                        };
+                    }
 
-					public void remove() {
-						throw new UnsupportedOperationException();
-					}
-				};
-			}
+                    public void remove() {
+                        throw new UnsupportedOperationException();
+                    }
+                };
+            }
 
-			@Override
-			public int size() {
-				return map.size();
-			}
-		};
-	}
+            @Override
+            public int size() {
+                return map.size();
+            }
+        };
+    }
 
-	public final synchronized boolean isEmpty() {
-		return size() == 0;
-	}
+    public final synchronized boolean isEmpty() {
+        return size() == 0;
+    }
 
-	public synchronized final V put(K key, V value) {
-		keyIndexList.clear();
-		isIndexUpToDate = false;
-		V v = putIntoMap(key, value);
-		if (v != null) {
-			fireValueChanged();
-		} else {
-			fireValueChanged();
-		}
-		return v;
-	}
+    public synchronized final V put(K key, V value) {
+        keyIndexList.clear();
+        isIndexUpToDate = false;
+        V v = putIntoMap(key, value);
+        if (v != null) {
+            fireValueChanged();
+        } else {
+            fireValueChanged();
+        }
+        return v;
+    }
 
-	public synchronized final void putAll(Map<? extends K, ? extends V> map) {
-		for (K k : map.keySet()) {
-			putIntoMap(k, map.get(k));
-		}
-		fireValueChanged();
-	}
+    public synchronized final void putAll(Map<? extends K, ? extends V> map) {
+        putAllIntoMap(map);
+        fireValueChanged();
+    }
 
-	public synchronized final V remove(Object key) {
-		keyIndexList.clear();
-		isIndexUpToDate = false;
-		V v = removeFromMap(key);
-		fireValueChanged();
-		return v;
-	}
+    protected synchronized void putAllIntoMap(Map<? extends K, ? extends V> map) {
+        for (K k : map.keySet()) {
+            putIntoMap(k, map.get(k));
+        }
+    }
 
-	public synchronized final Collection<V> values() {
-		buildIndexIfNecessary();
-		final AbstractMapMatrix<K, V> map = this;
-		return new AbstractCollection<V>() {
-			@Override
-			public Iterator<V> iterator() {
-				return new Iterator<V>() {
-					Iterator<K> it = keyIndexList.iterator();
+    public Map<K, V> getAll(K... keys) {
+        Map<K, V> result = new HashMap<>();
+        for (K key : keys) {
+            result.put(key, get(key));
+        }
+        return result;
+    }
 
-					public boolean hasNext() {
-						return it.hasNext();
-					}
+    public synchronized final V remove(Object key) {
+        keyIndexList.clear();
+        isIndexUpToDate = false;
+        V v = removeFromMap(key);
+        fireValueChanged();
+        return v;
+    }
 
-					public V next() {
-						return get(it.next());
-					}
+    public synchronized final Collection<V> values() {
+        buildIndexIfNecessary();
+        final AbstractMapMatrix<K, V> map = this;
+        return new AbstractCollection<V>() {
+            @Override
+            public Iterator<V> iterator() {
+                return new Iterator<V>() {
+                    Iterator<K> it = keyIndexList.iterator();
 
-					public void remove() {
-						throw new UnsupportedOperationException();
-					}
-				};
-			}
+                    public boolean hasNext() {
+                        return it.hasNext();
+                    }
 
-			@Override
-			public int size() {
-				return map.size();
-			}
-		};
-	}
+                    public V next() {
+                        return get(it.next());
+                    }
 
-	public final void clear() {
-		keyIndexList.clear();
-		isIndexUpToDate = false;
-		clearMap();
-	}
+                    public void remove() {
+                        throw new UnsupportedOperationException();
+                    }
+                };
+            }
 
-	protected abstract void clearMap();
+            @Override
+            public int size() {
+                return map.size();
+            }
+        };
+    }
 
-	protected abstract V removeFromMap(Object key);
+    public final synchronized void clear() {
+        keyIndexList.clear();
+        isIndexUpToDate = false;
+        clearMap();
+    }
 
-	protected abstract V putIntoMap(K key, V value);
+    protected abstract void clearMap();
+
+    protected abstract V removeFromMap(Object key);
+
+    protected abstract V putIntoMap(K key, V value);
 }
